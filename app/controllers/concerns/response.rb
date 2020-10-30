6ef178c855bcf394793e4b9482a82f2556a314d5
donnymays <dtmays2@gmail.com>
2020-10-30 09:28:19 -0700

    move json_response method to concerns directory

---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,5 @@
+module Resonse
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+end
--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -3,7 +3,5 @@ class ParksController < ApplicationController
   end
 
   private
-  def json_response(object, status = :ok)
-    render json: object, status: status
-  end
+
 end
