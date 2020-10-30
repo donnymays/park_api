34b78f2b380af566f0e2a9562470c4a6e659d32f
donnymays <dtmays2@gmail.com>
2020-10-30 10:07:13 -0700

    add scopes to park model

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,3 +1,9 @@
 class Park < ApplicationRecord
   validates :name, :type_name, :description, :state_name, :amenities, :entrace_fee, presence: true
+
+  scope :name, -> (park_name) { where("name ilike ?", "%#{park_name}%") }
+
+  scope :type_name, -> (type_name_input) { where("type_name ilike ?", "%#{type_name_input}") }
+
+  scope :random_park, -> (park_array) { where("park ilike ?", "#{park_array.sample}") }
 end
